289449560110151060728616741b4bb7686032fe
Arina Victor <arinavictor@yahoo.com>
2019-08-18 18:09:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def summon_captain_planet(planeteer_calls)
  new_calls = planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls(short_words)
+def long_planeteer_calls(planeteer_calls)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here

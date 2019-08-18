cf3958110b770ebc159e7160f780f192bbd43e96
Arina Victor <arinavictor@yahoo.com>
2019-08-18 18:05:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ name_array.each_with_index { |name, i| puts "#{i+1} #{name}" }
 
 end 
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
